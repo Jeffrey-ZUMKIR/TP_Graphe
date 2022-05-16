0d9b098388f96dee234181dbc2d3426e1ed49040
Osef1712 <j.zumkir@ludus-academie.com>
2022-05-16 17:50:53 +0200

    Debut TP 26

---
+++ TP_Graphe_26/TP_Graphe_26/define.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/*typedef struct Sommet {
+	unsigned int id;
+	int n_val;
+	int marqued;
+}Sommet;
+
+typedef struct Reseau {
+	int n, m;
+	Sommet* tabSom;
+	char** matrice;
+}Reseau;*/
+
+typedef struct CellSommet {
+	int n_val;
+	struct CellArc* liste_arcs; //Liste des arcs du sommet
+	struct CellSommet* suivant; //Pointeur vers sommet suivant
+}TypeSommet;
+
+typedef struct CellArc {
+	struct CellSommet* extremite; //pointeur vers successeur
+	struct CellArc* suivant; //pointeur vers arc suivant
+}TypeArc;
+
+typedef TypeSommet* TypeGraphe;
---
+++ TP_Graphe_26/TP_Graphe_26/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "define.h"
+
+using namespace std;
+
+
+int main() {
+	
+
+	return 0;
+}
+
